2bdce9ee55858870a2aae910f194217e30f162bb
awadh <awadh.hassan@student.moringaschool.com>
2022-08-15 22:44:01 +0300

    assigment complete

--- index.js
+++ index.js
@@ -1,11 +1,24 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(ride){
+  if (ride >2500){
+    return 'No can do.'
+  } else if (ride > 2000){
+    return 'I will gladly take your thirty bucks.';
+  } else if ( ride <= 400){
+    return 'This one is on me!'
+  }
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(city){
+  return city ==='NYC' ? 'Ok, sounds good.':'No go.';
+
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(get){
+  if (get === 'generous') {
+    return 'Thank you so much.';
+  }else if (get ==='not generous'){
+    return 'Thank you.';
+  }else{
+    return 'Bye.'
+  }
 }
